81bb1f0f8d6c9f86334ad0548d4bbf350e1701c8
Joseph <jym@berkeley.edu>
2018-04-25 18:15:28 -0700

    better cori script

--- cori.sh
+++ cori.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 module load python
+# sudo apt-get install python-pip python-dev nginx
+# pip install --user gunicorn flask
 pip install --user pulp
 python minesweeper.py --deploy
+ip="$(ip route get 8.8.8.8 | awk '{print $NF; exit}')"
+echo "App hosted at $ip:5000"
