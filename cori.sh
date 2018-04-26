4c813ddfe889aca156577eed75e13b1b043e9ec8
Joseph <jym@berkeley.edu>
2018-04-25 18:16:17 -0700

    fix order

--- cori.sh
+++ cori.sh
@@ -3,6 +3,6 @@ module load python
 # sudo apt-get install python-pip python-dev nginx
 # pip install --user gunicorn flask
 pip install --user pulp
-python minesweeper.py --deploy
 ip="$(ip route get 8.8.8.8 | awk '{print $NF; exit}')"
-echo "App hosted at $ip:5000"
+echo "Hosting app at $ip:5000"
+python minesweeper.py --deploy
